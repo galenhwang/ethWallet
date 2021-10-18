70eb3335f9c0f9d51d92b85f51af012567bdef5e
Galen Wang <galenhwang@yahoo.ca>
2021-10-18 09:37:43 -0500

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import styled from "styled-components";
+import ConnectWallet from "../components/ConnectWallet";
+import { useEthers } from "@usedapp/core";
+import SendTransaction from "../components/SendTransaction";
+
+const Home: NextPage = () => {
+  const account = null;
+  //implement logic to find if account is logged in or not
+  return account == null ? (
+    <ConnectWallet></ConnectWallet>
+  ) : (
+    <SendTransaction></SendTransaction>
+  );
+};
+
+export default Home;
