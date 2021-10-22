e44a3c42bb241d52d143a17fb05bf23c3e7c71e1
Galen Wang <galenhwang@yahoo.ca>
2021-10-21 21:55:04 -0700

    Implemented connect, display, and send functionality
    I didn't complete any styling, and there's a lingering bug I believe
    where it's' not connecting to the wallet properly. I will fix this as next
    TODOs

--- components/ConnectWallet.tsx
+++ components/ConnectWallet.tsx
@@ -1,8 +1,25 @@
 import styled from "styled-components";
 import { useEthers } from "@usedapp/core";
 
+const onError = (error: Error) => {
+  console.log(error.message) // for debugging purposes as I couldn't connect; the message was No Ethereum provider was found on window.ethereum.
+}
+
 const ConnectWallet: React.FC = () => {
-  //implement logic and button to connect wallet to dapp
-  return <></>;
+  const { activateBrowserWallet } = useEthers();
+  //implement logic and button to connect wallet to dapp = DONE
+  return (
+    <Button onClick={() => activateBrowserWallet(onError)}>Connect</Button>
+  )
 };
+
+const Button = styled.button`
+  /* Default; didn't have time to style */
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;`
+
 export default ConnectWallet;
+export { Button };
--- components/SendTransaction.tsx
+++ components/SendTransaction.tsx
@@ -1,24 +1,55 @@
 import styled from "styled-components";
 import { formatEther, parseEther } from "@ethersproject/units";
-import { useEtherBalance, useEthers, useSendTransaction } from "@usedapp/core";
+import { BigNumber } from 'ethers'
+import { useEtherBalance, useEthers, useSendTransaction, useLookupAddress } from "@usedapp/core";
 import { useEffect, useState } from "react";
 import head from "next/head";
+import { Button } from "../components/ConnectWallet";
 
 const SendTransaction: React.FC = () => {
-  //implement logic to display address of logged in wallet
-  //implement logic to display eth balance of logged in wallet
-  //implement logic to take in an input of a wallet address and state to hold it
-  //implement logic to take in an input of a amount to send and state to hold it
-  //implement logic for the button to send a transaction with the current values of the wallet
-  //address state and the amount state
-  const handleClick = () => {};
+  const { account } = useEthers()
+  const balance  = useEtherBalance(account)
+  const [address, setAddress] = useState("init")
+  const [amount, setAmount] = useState("init")
+  const [disabled, setDisabled] = useState(false)
+  const { sendTransaction, state } = useSendTransaction({ transactionName: 'Send Ethereum' })
+  useEffect(() => {
+    if (state.status != 'Mining') {
+      setDisabled(false)
+      setAmount('0')
+      setAddress('')
+    }
+  }, [state])
+  //implement logic to display address of logged in wallet = DONE
+  //implement logic to display eth balance of logged in wallet = DONE
+  //implement logic to take in an input of a wallet address and state to hold it = DONE
+  //implement logic to take in an input of a amount to send and state to hold it = DONE
+  //implement logic for the button to send a transaction with the current values of the wallet = DONE
+  //address state and the amount state = DONE
+  const handleClick = () => {
+    setDisabled(true)
+    sendTransaction({ to: address, value: parseEther(amount) })
+  };
 
   return (
     <>
+      <p>Your wallet address: {useLookupAddress()}</p>
+      <p>Balance is: {formatEther(balance ?? BigNumber.from("0"))}</p>
+      <p>Wallet to send to: <Input type="text" value={address} onChange={e => setAddress(e.target.value)} disabled={disabled}></Input></p>
+      <p>Amount to send: <Input type="number" value={amount} onChange={e => setAmount(e.target.value)} disabled={disabled}></Input></p>
+      <Button onClick={handleClick} disabled={disabled}>Send</Button>
       {" "}
       <div></div>{" "}
     </>
   );
 };
+
+const Input = styled.input`
+  padding: 0.5em;
+  margin: 0.5em;
+  color: palevioletred;
+  background: papayawhip;
+  border: none;
+  border-radius: 3px;`
 
 export default SendTransaction;
